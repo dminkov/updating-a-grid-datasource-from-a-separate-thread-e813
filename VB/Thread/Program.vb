950ef1b616a77727e990d7c1b4a600ec126f41f1
DevExpressExampleBot <fakeEmail>
2018-04-25 00:05:44 +0300

    Source auto update [skip ci]

--- VB/Thread/Program.vb
+++ VB/Thread/Program.vb
@@ -12,24 +12,24 @@
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E813
 
-
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.Windows.Forms
 
 Namespace Thread
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Class
+    Friend NotInheritable Class Program
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread> _
+        Shared Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Application.Run(New Form1())
+        End Sub
+    End Class
 End Namespace
